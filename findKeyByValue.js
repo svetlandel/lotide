65750a796617199d8dddc286ef1aae444083f7f7
Svitlana Taran <taran.svetlana@outlook.com>
2021-11-17 23:14:53 +0000

    adding the findKeyByValue file

---
+++ findKeyByValue.js
@@ -0,0 +1,18 @@
+function assertEqual (actual, expected) {
+    if (actual === expected) {
+      console.log("Assertion Passed: " + actual + "===" + expected);
+    } else {
+      console.log("Assertion Failed: " + actual + "!==" + expected);
+    }
+  };
+
+ function findKeyByValue(obj, search_value){
+     for(let key of Object.keys(obj)){
+         var value = obj[key];
+         if(value===search_value)
+         {
+             return key;
+         }
+     }
+     return undefined;
+ }
