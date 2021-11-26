c7c53edca4361ac9f8d4e67156bc34375479a267
Svitlana Taran <taran.svetlana@outlook.com>
2021-11-26 02:43:25 +0000

    adding the middle.js and assertArraysEqual.js files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,10 @@
 const eqArrays = require('./eqArrays');
 
-    function assertArrayEqual (array1, array2) {
+    function assertArraysEqual (array1, array2) {
         if (eqArrays(array1, array2)===true) {
           console.log("Arrays match: " + "array#1: ["+ array1 +  "] " + "array#2: [" + array2 + "]");
         } else {
           console.log("Arrays do not match: " + "array#1: ["+ array1 + "] " + " array#2: ["+ array2 + "]");
         }
       };
-      module.exports = assertArrayEqual;
+      module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,36 +1,19 @@
-function eqArrays( array1, array2) {
-    if (array1.length !== array2.length)
-    {
-        return false;
-    }else {
-        for (let i = 0; i < array1.length; i++) {
-            if (array1[i]!== array2[i]){  
-                return false;           
-        }
-    }
-    }
-  return true;
-    }
-
-    function assertArrayEqual (array1, array2) {
-        if (eqArrays(array1, array2)===true) {
-          console.log("Arrays match: " + "array#1: ["+ array1 +  "] " + "array#2: [" + array2 + "]");
-        } else {
-          console.log("Arrays do not match: " + "array#1: ["+ array1 + "] " + " array#2: ["+ array2 + "]");
-        }
-      };
-      
+const assertArraysEqual = require('./assertArraysEqual');
+//const eqArrays = require('./eqArrays');      
       function middle(array){
-        var newArray = [];
+        let newArray = [];
         if(array.length <= 2) {
-            console.log(newArray);
             return newArray = [];
         } else if(array.length%2 === 0) {
-            var middle1 = array[Math.floor((array.length - 1) / 2)];
-            var middle2 = array[Math.floor((array.length + 1) / 2)];
-            return newArray.push(middle1, middle2);
+            let middle1 = array[Math.floor((array.length - 1) / 2)];
+            let middle2 = array[Math.floor((array.length + 1) / 2)];
+            newArray.push(middle1, middle2);
+            return newArray;
+           
         } else{
-            var middle = array[Math.floor(array.length / 2)];
-            return newArray.push(middle);
+            let middle = array[Math.floor(array.length / 2)];
+            newArray.push(middle);
+            return newArray;
         }
-    }
+    };
+    module.exports = middle;
